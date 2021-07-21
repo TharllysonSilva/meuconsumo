328754efd1fffe1f10b32b7943854109a4733736
TharllysonSilva <tharllysonsilva@gmail.com>
2021-07-21 09:56:56 -0300

    barra lateral original

--- barra-lateral.php
+++ barra-lateral.php
@@ -1,5 +1,5 @@
 <!-- Barra Esquerda -->
-<!--<aside id="left-panel" class="left-panel">
+<aside id="left-panel" class="left-panel">
   <nav class="navbar navbar-expand-sm navbar-default">
 
     <div class="navbar-header">
@@ -14,14 +14,14 @@
         <li>
           <a href="home.php" style="font-weight:700;margin:5px 0px"> <i class="menu-icon fa fa-home" style="width:11px;margin-right:5px;"></i>Home </a>
         </li>
-        <h3 class="menu-title" style="padding:10px 0px; margin:0px 0px"><i class="menu-icon fa fa-bar-chart-o"></i> Relatório</h3><!-- /.menu-title --
+        <h3 class="menu-title" style="padding:10px 0px; margin:0px 0px"><i class="menu-icon fa fa-bar-chart-o"></i> Relatório</h3><!-- /.menu-title -->
         <li>
           <a href="relatoriodiario.php"> Diário </a>
         </li>
         <li>
           <a href="relatoriomensal.php"> Mensal </a>
         </li>
-        <h3 class="menu-title" style="padding:10px 0px;margin:10px 0px 0px 0px;"><i class="menu-icon fa fa-gear" style="margin-right: 5px"></i>Configurações</h3><!-- /.menu-title --
+        <h3 class="menu-title" style="padding:10px 0px;margin:10px 0px 0px 0px;"><i class="menu-icon fa fa-gear" style="margin-right: 5px"></i>Configurações</h3><!-- /.menu-title -->
         <li>
           <a href="alterarsenha.php">Alterar Senha </a>
         </li>
@@ -35,103 +35,7 @@
           <a href="sair.php" style="font-weight:700;margin:10px 0px 0px 0px;"> <i class="menu-icon fa fa-power-off" style="width: 11px; margin-right: 5px;"></i>Sair </a>
         </li>
       </ul>
-    </div><!-- /.navbar-collapse --
+    </div><!-- /.navbar-collapse -->
   </nav>
 </aside><!-- Fim Barra Esquerda -->
-<html>
-  <head>
- 
-  </head>
-  <body><div class="area"></div><nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="http://justinfarrow.com">
-                        <i class="fa fa-home fa-2x"></i>
-                        <span class="nav-text">
-                            Dashboard
-                        </span>
-                    </a>
-                  
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                        <i class="fa fa-laptop fa-2x"></i>
-                        <span class="nav-text">
-                            Stars Components
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
-                            Forms
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-folder-open fa-2x"></i>
-                        <span class="nav-text">
-                            Pages
-                        </span>
-                    </a>
-                   
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-bar-chart-o fa-2x"></i>
-                        <span class="nav-text">
-                            Graphs and Statistics
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-font fa-2x"></i>
-                        <span class="nav-text">
-                           Quotes
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                       <i class="fa fa-table fa-2x"></i>
-                        <span class="nav-text">
-                            Tables
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                        <i class="fa fa-map-marker fa-2x"></i>
-                        <span class="nav-text">
-                            Maps
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-info fa-2x"></i>
-                        <span class="nav-text">
-                            Documentation
-                        </span>
-                    </a>
-                </li>
-            </ul>
 
-            <ul class="logout">
-                <li>
-                   <a href="#">
-                         <i class="fa fa-power-off fa-2x"></i>
-                        <span class="nav-text">
-                            Logout
-                        </span>
-                    </a>
-                </li>  
-            </ul>
-        </nav>
-  </body>
-    </html>
